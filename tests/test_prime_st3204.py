6fbf7bf87333dc4cfe4e35ae656c24812be97d17
billytan430 <st3204@columbia.edu>
2023-10-31 15:24:01 -0400

    Added test for is_prime and pytest as a dev dependency

--- tests/test_prime_st3204.py
+++ tests/test_prime_st3204.py
@@ -1,1 +1,11 @@
-from prime_st3204 import prime_st3204
+from prime_st3204 import is_prime
+
+def test_is_prime():
+    assert not is_prime(0)
+    assert not is_prime(1)
+    assert is_prime(2)
+    assert is_prime(3)
+    assert not is_prime(4)
+    assert is_prime(5)
+    assert not is_prime(15)
+    assert is_prime(17)
